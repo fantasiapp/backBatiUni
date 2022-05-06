67646e152950613ffd957253287503ea4b214ef6
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-05-06 18:06:28 +0200

    bug modifyDetailedPost 5

--- backBatiUni/modelData/dataAccessor.py
+++ backBatiUni/modelData/dataAccessor.py
@@ -339,25 +339,23 @@ class DataAccessor():
     unset = data["unset"] if "unset" in data else False
     data = data["detailedPost"]
     date = datetime.strptime(data["date"], "%Y-%m-%d") if "date" in data and data["date"] else None
-    detailedPost = DetailedPost.objects.get(id=data["id"])
-    PorM = detailedPost.Post if detailedPost.Post else detailedPost.Mission
-    if detailedPost and not unset:
+    detailedPost = DetailedPost.objects.filter(id=data["id"])
+    if detailedPost:
       detailedPost = detailedPost[0]
-      for field in ["content", "validated", "refused"]:
-        if field in data:
-          setattr(detailedPost, field, data[field])
-      detailedPost.save()
-      if date:
-        if not unset:
+      PorM = detailedPost.Post if detailedPost.Post else detailedPost.Mission
+      if not unset:
+        if date:
           dateNowString = detailedPost.date.strftime("%Y-%m-%d") if detailedPost.date else None
           if dateNowString != data["date"]:
             detailedPost = DetailedPost.objects.create(Post=detailedPost.Post, Mission=detailedPost.Mission, content=detailedPost.content, date=date, validated=detailedPost.validated)
-        else:
-          if Supervision.objects.filter(DetailedPost=detailedPost):
-            return {"modifyDetailedPost":"Warning", "messages":f"Cette tâche du {data['date']} est commentée"}
-          detailedPost.delete()
-          PorM = detailedPost.Post if detailedPost.Post else detailedPost.Mission
-          return {"modifyDetailedPost":"OK", PorM.id:PorM.computeValues(PorM.listFields(), currentUser, True)}
+        for field in ["content", "validated", "refused"]:
+          if field in data:
+            setattr(detailedPost, field, data[field])
+        detailedPost.save()
+      else:
+        if Supervision.objects.filter(DetailedPost=detailedPost):
+          return {"modifyDetailedPost":"Warning", "messages":f"Cette tâche du {data['date']} est commentée"}
+        detailedPost.delete()
       return {"modifyDetailedPost":"OK", PorM.id:PorM.computeValues(PorM.listFields(), currentUser, True)}
     return {"modifyDetailedPost":"Error", "messages":f"No Detailed Post with id {data['id']}"}
 
