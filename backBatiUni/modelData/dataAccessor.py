73acdf2e9b440e5f29957c8154fd2de6e38a7b05
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-05-24 15:23:23 +0200

    certificate without date trace 2

--- backBatiUni/modelData/dataAccessor.py
+++ backBatiUni/modelData/dataAccessor.py
@@ -789,7 +789,7 @@ class DataAccessor():
 
   @classmethod
   def __uploadFile(cls, data, currentUser):
-    print(data)
+    print("data", data["ext"], data["name"], data["nature"], data["expirationDate"])
     if not "ext" in data:
       return {"uploadFile":"Warning", "messages":f"Aucune image n'est associé à la demande"}
     if not data['ext'] in File.authorizedExtention:
