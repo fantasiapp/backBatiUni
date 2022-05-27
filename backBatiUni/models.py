7dadb757c9288e09504e4958df24de27aa1287bc
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-05-27 10:38:51 +0200

    bug correction reamStructure Candidate

--- backBatiUni/models.py
+++ backBatiUni/models.py
@@ -703,7 +703,7 @@ class Candidate(CommonModel):
   def generateRamStructure(cls):
     RamData.ramStructure["Post"]["Candidate"] = deepcopy(RamData.allPost)
     for candidate in Candidate.objects.all():
-      if candidate.Post:
+      if candidate.Post and not isinstance(candidate.Post, Mission):
         print("generateRamStructure", candidate.Post, candidate.id)
         RamData.ramStructure["Post"]["Candidate"][candidate.Post.id].append(candidate.id)
 
