d0eacc96aeb8eab01abc8439332874bfc36f9768
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-06-09 14:02:32 +0200

    uploadFile 2

--- tests/backBatiUni.py
+++ tests/backBatiUni.py
@@ -16,7 +16,7 @@ userName, password = "st", "pwd"
 # userName, password = "jeanluc.walter@fantasiapp.com", "123456Aa"
 address = 'http://localhost:8000'
 query = "token"
-numberCompanies = 50
+numberCompanies = 10
 emailList, missionList, emailListPME, emailListST, detailedPost = {}, {}, [], [], {}
 
 arguments = sys.argv
