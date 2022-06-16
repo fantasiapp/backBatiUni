fde5296d21468d96e74bd9183ba0b8ec07fa0767
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-06-16 11:57:45 +0200

    notation notifiation trace

--- backBatiUni/modelData/dataAccessor.py
+++ backBatiUni/modelData/dataAccessor.py
@@ -896,13 +896,13 @@ class DataAccessor():
       listMission = [(candidate.Mission.quality + candidate.Mission.security + candidate.Mission.organisation) / 3 for candidate in Candidate.objects.filter(Company = subContractor, isChoosen = True) if candidate.Mission.isClosed]
       subContractor.starsST = round(sum(listMission)/len(listMission)) if len(listMission) else 0
       Notification.createAndSend(Company=company, subContractor=subContractor, title="Modification de la mission", nature="PME", Role="ST", content=f"La société {company.name} vient de vous évaluer.", timestamp=datetime.now().timestamp())
-      print("new evaluation st")
+      print("new evaluation st", company.id, subContractor.id)
       subContractor.save()
     else:
       Notification.createAndSend(Company=subContractor, subContractor=company, title="Modification de la mission", nature="ST", Role="PME", content=f"La société {candidate[0].Company.name} vient de vous évaluer.", timestamp=datetime.now().timestamp())
       listMission = [(mission.vibeST + mission.securityST + mission.organisationST) / 3 for mission in Mission.objects.filter(Company=company, isClosed=True)]
       company.starsPME = round(sum(listMission)/len(listMission)) if len(listMission) else 0
-      print("new evaluation pme")
+      print("new evaluation pme", company.id, subContractor.id)
       company.save()
 
   @classmethod
