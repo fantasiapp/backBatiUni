530d6dc18f3ac42a9edc38ad8857aad0ccfff828
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-06-16 15:38:01 +0200

    change login bug

--- backBatiUni/modelData/dataAccessor.py
+++ backBatiUni/modelData/dataAccessor.py
@@ -1127,10 +1127,10 @@ class DataAccessor():
   @classmethod
   def __changeUserProfileName(cls, user, userProfile, newUserName):
     print("__changeUserProfileName", userProfile, newUserName)
-    sameLogin = UserProfile.objects.filter(userNameInternal=user)
+    sameLogin = User.objects.filter(userName=newUserName)
     if len(sameLogin) == 1:
       print("inside loop", sameLogin, sameLogin[0].id)
-      if sameLogin[0].id == userProfile.id: return None
+      if sameLogin[0].id == user.id: return None
       return f"Cet email est déjà utilisé."
     print("oldValues", user.userName, userProfile.email)
     user.userName = newUserName
