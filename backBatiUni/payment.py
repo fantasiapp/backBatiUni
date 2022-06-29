75ddadd5d66980c7651afb923745ebe1ea9b36eb
wlorgere <williamlorgere@gmail.com>
2022-06-29 11:19:46 +0200

    fix bug

--- backBatiUni/payment.py
+++ backBatiUni/payment.py
@@ -12,12 +12,12 @@ class Payment():
 
             # Create a PaymentIntent with the order amount and currency
             intent = stripe.PaymentIntent.create(
-                amount=1,
+                amount=1000,
                 currency='eur',
                 automatic_payment_methods={
                 'enabled': True,
                 },
             )
-            return Response({"Payment":"OK", "clientSecret": intent["client_secret"]})
+            return {"Payment":"OK", "clientSecret": intent["client_secret"]}
         except Exception as e:
-            return Response({"Error": str(e)})
+            return {"Error": str(e)}
