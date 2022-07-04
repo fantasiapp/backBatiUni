738e00cc1f12156c13201b6ec33402c58a9fe4b4
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-07-04 11:02:07 +0200

    view in recommandation

--- backBatiUni/modelData/dataAccessor.py
+++ backBatiUni/modelData/dataAccessor.py
@@ -1419,8 +1419,8 @@ class DataAccessor():
     print("giveRecommandation", data)
     company = Company.objects.get(id=data["companyRecommanded"])
     name = "Un sous-traitant à la recherche d'une entreprise" if data['view'] == "ST" else "Une entreprise à la recherche de sous-traitances"
-    view = Role.objects.get(name = name)
-    if Recommandation.objects.filter(companyRecommanded=company, companyNameRecommanding=data['companyNameRecommanding'], view=view):
+    data['view'] = Role.objects.get(name = name)
+    if Recommandation.objects.filter(companyRecommanded=company, companyNameRecommanding=data['companyNameRecommanding'], view=data['view']):
       return {"giveRecommandation":"Warning", "messages":"La recommandation existe déjà"}
     kwargs = {"date":timezone.now()}
     for key, value in data.items():
