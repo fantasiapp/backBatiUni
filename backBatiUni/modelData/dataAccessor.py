4eafd76c7af4e52c2a12ad878b4847f96dd60993
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-07-04 17:19:43 +0200

    remove stripe

--- backBatiUni/modelData/dataAccessor.py
+++ backBatiUni/modelData/dataAccessor.py
@@ -136,21 +136,21 @@ class DataAccessor():
   @classmethod
   def __registerAction(cls, data, token):
 
-    print("stripe", stripe)
-    print("stripe api key", STRIPE_API_KEY)
-    stripe.api_key = STRIPE_API_KEY
+    # print("stripe", stripe)
+    # print("stripe api key", STRIPE_API_KEY)
+    # stripe.api_key = STRIPE_API_KEY
 
     print("registerAction", data)
     companyData = data['company']
 
-    if not "@" in data["email"]:
-      data["email"] += "@g.com" 
-    print("registerAction stripe", companyData["name"], data["email"])
+    # if not "@" in data["email"]:
+    #   data["email"] += "@g.com" 
+    # print("registerAction stripe", companyData["name"], data["email"])
     customer = stripe.Customer.create(name = "test django", description = "customer de test")
 
 
-    company = Company.objects.create(name=companyData['name'], address=companyData['address'], companyMail=data["email"], activity=companyData['activity'], ntva=companyData['ntva'], siret=companyData['siret'], stripeCustomerId = customer.id)
-    # company = Company.objects.create(name=companyData['name'], address=companyData['address'], companyMail=data["email"], activity=companyData['activity'], ntva=companyData['ntva'], siret=companyData['siret'], stripeCustomerId = "")
+    # company = Company.objects.create(name=companyData['name'], address=companyData['address'], companyMail=data["email"], activity=companyData['activity'], ntva=companyData['ntva'], siret=companyData['siret'], stripeCustomerId = customer.id)
+    company = Company.objects.create(name=companyData['name'], address=companyData['address'], companyMail=data["email"], activity=companyData['activity'], ntva=companyData['ntva'], siret=companyData['siret'], stripeCustomerId = "")
     cls.__getGeoCoordinates(company)
     company.Role = Role.objects.get(id=data['Role'])
     company.save()
