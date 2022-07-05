9f08a5e055f7dbdd011eef987dff596e0f350dfe
wlorgere <williamlorgere@gmail.com>
2022-07-05 18:18:24 +0200

    add more informations in payment response

--- backBatiUni/payment.py
+++ backBatiUni/payment.py
@@ -17,27 +17,28 @@ class PaymentManager():
         company = Company.objects.get(id = userProfile.Company.id)
         customerId = company.stripeCustomerId
 
-        def fetchAmount(product = ""):
-            if product:
-                stripeProduct = stripe.Product.retrieve(product)
-                price = stripe.Price.retrieve(stripeProduct.default_price)
-                return price.unit_amount
-            return 10000
-
         try:
             print("payment data", request.data)
+
+            stripeProduct = stripe.Product.retrieve(request.product)
+            price = stripe.Price.retrieve(stripeProduct.default_price)
 
             # Create a PaymentIntent with the order amount and currency
             intent = stripe.PaymentIntent.create(
                 customer = customerId,
                 setup_future_usage = "off_session",
-                amount=fetchAmount(),
+                amount=price.amount,
                 currency='eur',
                 automatic_payment_methods={
                 'enabled': True,
                 },
             )
-            return {"CreatePaylentIntent":"OK", "clientSecret": intent["client_secret"]}
+            return {
+                    "CreatePaylentIntent":"OK", 
+                    "clientSecret": intent["client_secret"], 
+                    "price": intent["price"],
+                    "productName": stripeProduct.name
+                    }
         except Exception as e:
             return {"Error": str(e)}
     
