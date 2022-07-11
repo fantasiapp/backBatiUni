1f8c3567b706f9b7da89382d3930e7c5eb47ffd1
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-07-11 18:30:45 +0200

    after merge

--- tests/backBatiUni.py
+++ tests/backBatiUni.py
@@ -284,7 +284,7 @@ def executeQuery():
       response = requests.post(url, headers=headers, json=post)
     elif query == "uploadFile":
       file1 = {'action':"uploadFile", "ext":"png", "name":"qualibat", "fileBase64":getDocStr(0), "nature":"labels", "expirationDate":"2022-02-12"}
-      file2 = {'action':"uploadFile", "ext":"png", "name":"kbis", "fileBase64":getDocStr(1), "nature":"admin", "expirationDate":"2022-02-12"}
+      file2 = {'action':"uploadFile", "ext":"png", "name":"Kbis", "fileBase64":getDocStr(1), "nature":"admin", "expirationDate":"2022-02-12"}
       file4 = {'action':"uploadFile", "ext":"svg", "name":"Document technique", "fileBase64":getDocStr(4), "nature":"post", "Post":2}
       file5 = {'action':"uploadFile", "ext":"jpg", "name":"Plan", "fileBase64":getDocStr(2), "nature":"post", "Post":2}
       requests.post(url, headers=headersPme, json=file2)
