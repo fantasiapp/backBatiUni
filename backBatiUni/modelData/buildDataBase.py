eada6f14a637fec764cdab7a19fefeb24a5a8ac2
wlorgere <williamlorgere@gmail.com>
2022-07-18 16:44:04 +0200

    add fetch prices request

--- backBatiUni/modelData/buildDataBase.py
+++ backBatiUni/modelData/buildDataBase.py
@@ -193,7 +193,7 @@ class CreateNewDataBase:
     self.connection = db.connect(
       user = os.getenv('DB_USERNAME'),
       password = os.getenv('DB_PASSWORD'),
-      host = os.getenv('DB_HOST'),
+      host = os.getenv('DB_HOST'),d
       database = os.getenv('DB_NAME'),
     )
     self.cursor = self.connection.cursor()
