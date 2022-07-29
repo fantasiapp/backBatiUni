f27fe37eaafb81723ffc8e3a0984ef1e651870ef
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-07-29 18:20:15 +0200

    start buildContract

--- backBatiUni/modelData/buildContract.py
+++ backBatiUni/modelData/buildContract.py
@@ -1,14 +1,19 @@
 from fpdf import FPDF
-
+from ..models import *
 class BuildContract(FPDF):
 
   def __init__(self, userProfile):
     self.userProfile = userProfile
+
     pdf = FPDF('P', 'mm', 'A4')
     pdf.add_page()
     pdf.set_font('Times', '', 12)
     pdf.cell(40, 10, 'Hello World!')
     pdf.output('./files/documents/tuto1.pdf', 'F')
 
-  # def header(self):
+  def header(self):
+      file = File.objects.filter(nature="userImage", Company=self.userProfile.Company)
+      if file:
+        file = file[0] 
+        print(file.path)
   #   self.image()
