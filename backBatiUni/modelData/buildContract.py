f7d4d5f4c71b09964c4af6333066fc36a542214d
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-08-01 17:56:17 +0200

    check old password

--- backBatiUni/modelData/buildContract.py
+++ backBatiUni/modelData/buildContract.py
@@ -2,33 +2,34 @@ from fpdf import FPDF
 from ..models import *
 class MyPdf(FPDF):
 
-  def __init__(self, userProfile):
-    self.userProfile = userProfile
+  def __init__(self):
+    self.userProfile = None
     print(self.userProfile.Company.name)
     file = File.objects.filter(nature="userImage", Company=self.userProfile.Company)
     print("buildContract", file)
 
   def header(self):
-      file = File.objects.filter(nature="userImage", Company=self.userProfile.Company)
-      if file:
-        file = file[0]
-        self.image(file.path, 10, 8, 33)
-        print("header", file.path)
-      self.set_font('Arial', 'B', 15)
-      # Move to the right
-      self.cell(80)
-      # Title
-      self.cell(30, 10, 'Title', 1, 0, 'C')
-      # Line break
-      self.ln(20)
-  #   self.image()
+    print("header")
+    file = File.objects.filter(nature="userImage", Company=self.userProfile.Company)
+    if file:
+      file = file[0]
+      self.image(file.path, 10, 8, 33)
+      print("header", file.path)
+    self.set_font('Arial', 'B', 15)
+    # Move to the right
+    self.cell(80)
+    # Title
+    self.cell(30, 10, 'Title', 1, 0, 'C')
+    # Line break
+    self.ln(20)
 
 class BuildContract:
 
   def __init__(self, userProfile):
-    pdf = MyPdf(userProfile)
+    pdf = MyPdf('P', 'mm', 'A4')
+    pdf.userProfile = userProfile
 
-    pdf.alias_nb_pages()
+    # pdf.alias_nb_pages()
     pdf.add_page()
     pdf.set_font('Times', '', 12)
     for i in range(1, 41):
