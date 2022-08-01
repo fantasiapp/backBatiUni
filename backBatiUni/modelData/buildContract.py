e3a492792349ca167af8aa9a22683e12219d52d1
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-08-01 17:18:57 +0200

    check old password

--- backBatiUni/modelData/buildContract.py
+++ backBatiUni/modelData/buildContract.py
@@ -1,18 +1,12 @@
 from fpdf import FPDF
 from ..models import *
-class BuildContract(FPDF):
+class MyPdf(FPDF):
 
   def __init__(self, userProfile):
     self.userProfile = userProfile
     print(self.userProfile.Company.name)
     file = File.objects.filter(nature="userImage", Company=self.userProfile.Company)
     print("buildContract", file)
-
-    pdf = FPDF('P', 'mm', 'A4')
-    pdf.add_page()
-    pdf.set_font('Times', '', 12)
-    pdf.cell(40, 10, 'Hello World!')
-    pdf.output('./files/documents/tuto1.pdf', 'F')
 
   def header(self):
       file = File.objects.filter(nature="userImage", Company=self.userProfile.Company)
@@ -28,3 +22,15 @@ class BuildContract(FPDF):
       # Line break
       self.ln(20)
   #   self.image()
+
+class BuildContract:
+
+  def __init__(self, userProfile):
+    pdf = MyPdf('P', 'mm', 'A4', userProfile=userProfile)
+
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_font('Times', '', 12)
+    for i in range(1, 41):
+      pdf.cell(0, 10, 'Printing line number ' + str(i), 0, 1)
+    pdf.output('./files/documents/tuto1.pdf', 'F')
