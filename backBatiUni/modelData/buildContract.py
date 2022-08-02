56383d92ddc1cf1c2e7a8c45f4ba770683183ead
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-08-02 19:14:36 +0200

    contract part1

--- backBatiUni/modelData/buildContract.py
+++ backBatiUni/modelData/buildContract.py
@@ -66,7 +66,7 @@ class BuildContract:
   @property
   def __findCapital(self):
     if self.userProfile.Company.capital:
-      return f'au capital de {"{self.userProfile.Company.capital:,}".replace(",", " ")} €'
+      return f'au capital de {"{self.userProfile.Company.capital:,}".replace(",", " ")} euros'
     return ""
 
 
