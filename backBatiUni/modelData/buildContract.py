c2739104e0fc636d7c957a400df63afad25f5336
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-08-02 11:08:30 +0200

    create contract

--- backBatiUni/modelData/buildContract.py
+++ backBatiUni/modelData/buildContract.py
@@ -23,7 +23,8 @@ class MyPdf(FPDF):
     # Move to the right
     self.cell(60, 40)
     # Title
-    self.cell(30, 20, 'ACCORD – CADRE DE SOUS-TRAITANCE', 1, 0, 'C')
+    # self.cell(30, 20, 'ACCORD – CADRE DE SOUS-TRAITANCE', 1, 0, 'C')
+    self.write_html("<B>hello</B> <I>world äöü &auml;</I>", font="Arial", newline=True)
     # Line break
     self.ln(20)
 
